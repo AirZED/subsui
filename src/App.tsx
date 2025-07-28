e010efb85b541aea518e2705785ad4b5ced58cdf
Mfoniso Ukpabio <regnalukpabio@gmail.com>
2025-07-28 01:11:43 +0100

    updated landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage.js";
 import TicketPage from "./pages/Ticketpage.js";
 import EventRegistrationPage from "./pages/events-page.tsx";
 
 import TicketVerification from "./pages/TicketVerification.js";
 import UpcomingEvents from "./pages/Upcoming-events.tsx";
 import EventCreated from "./pages/event-created.tsx";
-import Home from "@pages/new-home.tsx"
+import Home from "@/pages/home.tsx"
 import { Header } from "@components/Header.tsx";
 
 
